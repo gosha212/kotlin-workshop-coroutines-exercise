c62e5d992046ef75db2f24cb8d3b4bf86ec9e958
Georgy Steshin <georgys@wix.com>
2024-03-19 18:15:33 +0200

    Solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,16 +16,30 @@ private val uiThread = Executors.newSingleThreadExecutor {
     Thread(it, "custom-main").apply { isDaemon = true }
 }.asCoroutineDispatcher()
 
-fun main() {
+fun main() = runBlocking(uiThread) {
     // 1. Set progress visibility to true on ui thread (setProgressVisibility)
+    setProgressVisibility(true)
+    // 2. Execute 50 network request on network pool
+    // (executeNetworkRequest)
+    val networkResult: Int = withContext(networkDispatcher) {
+        val results = (1..50).map {
+            async {
+                executeNetworkRequest()
+            }
+        }
 
-    // 2. Execute 50 network request on network pool (executeNetworkRequest)
+        results.awaitAll().sum()
+    }
 
     // 3. Sum the network responses and insert the result to database on database thread (addDataToDb).
+    val dbResult = withContext(dataBaseDispatcher) {
+        addDataToDb(networkResult)
+    }
 
     // 4. Set progress visibility to false on ui thread (setProgressVisibility)
-
+    setProgressVisibility(false)
     // 5. Print the data that was returned from the addToDb function on the ui thread (printData)
+    printData(dbResult)
 }
 
 
