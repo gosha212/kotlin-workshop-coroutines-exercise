fc9a9724140e33347b8cdb5cfa3ebd6bf3491dd6
Georgy Steshin <georgys@wix.com>
2024-03-20 20:45:11 +0200

    Solution 2

--- src/main/kotlin/Exercise2.kt
+++ src/main/kotlin/Exercise2.kt
@@ -16,17 +16,49 @@ private val uiThread = Executors.newSingleThreadExecutor {
 }.asCoroutineDispatcher()
 
 fun main() {
-    // 1. Set progress visibility to true on ui thread (setProgressVisibility)
+    runBlocking(uiThread) {
+        // 1. Set progress visibility to true on ui thread (setProgressVisibility)
+        setProgressVisibility(true)
 
-    // 2. Execute 50 network request on network pool (executeNetworkRequest)
+        val coroutineScope = CoroutineScope(networkDispatcher)
 
-    // 3. Cancel all network request after 500 ms
+        // 2. Execute 50 network request on network pool (executeNetworkRequest)
+        repeat(50) {
+            coroutineScope.launch {
+                executeNetworkRequest()
+            }
+        }
 
-    // 4. Execute 50 failing network request on network pool (executeNetworkRequestWithError)
+        // 3. Cancel all network request after 500 ms
+        delay(500)
+        coroutineScope.cancel()
 
-    // 5. Sum the successful responses and insert the result to database on database thread (addDataToDb).
+        // 4. Execute 50 failing network request on network pool (executeNetworkRequestWithError)
+        val deferredList = supervisorScope {
+            (1..50).map {
+                async(networkDispatcher) {
+                    executeNetworkRequestWithError()
+                }
+            }
+        }
 
-    // 6. Set progress visibility to false on ui thread (setProgressVisibility)
+        val successfulResults: List<Int> = deferredList.map {
+            try {
+                it.await()
+            } catch (e: Exception) {
+                log("Error: ${e.message}")
+                0
 
-    // 7. Print the data that was returned from the addToDb function on the ui thread (printData)
+            }
+        }
+
+        // 5. Sum the successful responses and insert the result to database on database thread (addDataToDb).
+        val result = withContext(dataBaseDispatcher) {
+            addDataToDb(successfulResults.sum())
+        }
+        // 6. Set progress visibility to false on ui thread (setProgressVisibility)
+        setProgressVisibility(false)
+        // 7. Print the data that was returned from the addToDb function on the ui thread (printData)
+        printData(result)
+    }
 }
